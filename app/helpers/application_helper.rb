2d8498d82214b8478fa210a959bafcdb3a02628e
Andrew Nesbitt <andrewnez@gmail.com>
2012-11-20 19:01:59 +0000

    Show some signed up users on the homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
         .gsub(/\#/, 'sharp')
         .parameterize
   end
+
+  def gravatar_url(digest, size = '80')
+    digest ||= ''
+    "https://secure.gravatar.com/avatar/#{digest}.png?s=#{size}&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"
+  end
 end
