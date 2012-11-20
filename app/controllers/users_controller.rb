8aa868de702d75502e8cd8c13332210bdf678325
Andrew Nesbitt <andrewnez@gmail.com>
2012-11-20 19:40:47 +0000

    Hacky :/

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    pulls = PullRequest.find_by_nickname(user.nickname)
+    
+    pulls = PullRequest.find_by_nickname(user.nickname) rescue []
     render :show, :locals => { :user => user, :pull_requests => pulls }
   end
 end
