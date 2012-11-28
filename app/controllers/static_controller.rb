542e38e92ab03206a22f45e0a26deba5cd0611f4
Baris Balic <baris.balic@forward.co.uk>
2012-11-28 10:38:46 +0000

    Added Quote model with hardcoded quote.  Example usage in the homepage.html.haml

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,5 +2,6 @@ class StaticController < ApplicationController
   def homepage
     @projects = Project.limit(100).sample(6)
     @users = User.limit(100).sample(40)
+    @quote = Quote.random
   end
 end
---
+++ app/models/quote.rb
@@ -0,0 +1,21 @@
+class Quote 
+  attr_reader :name, :twitter_handle, :github_handle, :company, :comment
+
+  def initialize(name, twitter_handle, github_handle, company, comment)
+    @name = name
+    @twitter_handle = twitter_handle
+    @github_handle = github_handle
+    @comment = company
+    @comment = comment
+  end
+  
+  def self.all
+    [
+      Quote.new('Bishop', '341-B', '341b', 'Weyland Wutani', "I may be synthetic but I'm not stupid.")
+      ]
+  end
+  
+  def self.random
+    all[rand(all.size)-1]
+  end
+end
