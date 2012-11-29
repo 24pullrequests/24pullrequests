55a93934a007e410daeec719b7214785618fd7c4
Andrew Nesbitt <andrewnez@gmail.com>
2012-11-29 23:19:10 +0000

    Show slightly more things on the homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   def homepage
-    @projects = Project.limit(100).sample(6)
-    @users = User.limit(100).sample(40)
+    @projects = Project.limit(100).sample(12)
+    @users = User.limit(100).sample(45)
     @quote = Quote.random
   end
 end
