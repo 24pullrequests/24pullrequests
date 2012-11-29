786ea8c3241cf1b10efb2fa76c81d99b4215ddc1
Andrew Nesbitt <andrewnez@gmail.com>
2012-11-29 23:18:59 +0000

    Only pull requests between 1st and 24th december plz

--- app/models/pull_request_downloader.rb
+++ app/models/pull_request_downloader.rb
@@ -16,6 +16,12 @@ class PullRequestDownloader
 
   def download_pull_requests
     events = github_client.user_events(user.nickname)
-    events.select{|e| e.type == 'PullRequestEvent' && e.payload.action == 'opened' }
+    events.select do |e| 
+      event_date = DateTime.parse(e['payload']['pull_request']['created_at']).strftime("%s").to_i
+      e.type == 'PullRequestEvent' && 
+      e.payload.action == 'opened' &&
+      event_date >= PullRequest::EARLIEST_PULL_DATE && 
+      event_date <= PullRequest::LATEST_PULL_DATE
+    end
   end
 end
