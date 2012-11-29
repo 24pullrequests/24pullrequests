535fad58cb94020c71cfd42f2d8a13789832d2bf
Andrew Nesbitt <andrewnez@gmail.com>
2012-11-29 23:30:58 +0000

    Sorry TeX, you were messing with my OCD!

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,5 +6,5 @@ class Project < ActiveRecord::Base
   LANGUAGES = ["ActionScript", "C", "C#", "C++", "Common Lisp", "CSS", "Diff", 
                "Emacs Lisp", "Erlang", "Haskell", "HTML", "Java", "JavaScript", 
                "Lua", "Objective-C", "Perl", "PHP", "Python", "Ruby", "Scala", 
-               "Scheme", "TeX", "XML"]
+               "Scheme", "XML"]
 end
