aa8379a0a12e94128b1933ad3829ac40a47e23c8
Tom Scott <tubbo@psychedeli.ca>
2012-11-30 18:48:40 -0500

    Project GitHub URLs must be fully qualified.
    - Validate that Project GitHub URLs begin with "http://"
    - Test validation in specs
    - Add Rake task for cleaning up old Projects without fully-qualified
      URLs. It should be run on the production server once after deploying
      the code in this Pull Request.
    - Add test data to db/seeds.rb for testing the Rake task. Verify that
      the task works in dev by typing `rake db:seed cleanup:projects`. All
      projects should be prefixed with one "http://", no matter how many
      times you run the task.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+# Test the cleanup script
+fully_qualified = Project.create \
+  description: 'this should have a fully qualified url',
+  github_url: 'http://github.com/fully/qualified',
+  name: 'qualified',
+  main_language: 'Ruby'
+relative = Project.new \
+  description: 'this should have a relative github url',
+  github_url: 'github.com/dontwork/relative_path',
+  name: 'relative_relative',
+  main_language: 'Ruby'
+relative.save(validate: false)
