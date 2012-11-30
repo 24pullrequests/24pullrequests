72b9fa50e1cbb0815077b6b1a61e025077dbd49f
Andrew Nesbitt <andrewnez@gmail.com>
2012-11-30 10:10:59 +0000

    Max length of description is 200

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
   attr_accessible :description, :github_url, :name, :main_language
 
   validates_presence_of :description, :github_url, :name, :main_language
+  validates_length_of :description, :within => 50..200
 
   LANGUAGES = ["ActionScript", "C", "C#", "C++", "Common Lisp", "CSS", "Diff", 
                "Emacs Lisp", "Erlang", "Haskell", "HTML", "Java", "JavaScript", 
