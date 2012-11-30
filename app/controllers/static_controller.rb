e62e33b5a78794b93cf173d866a859ec0be8617e
Andrew Nesbitt <andrewnez@gmail.com>
2012-11-30 22:05:45 +0000

    Use a slightly larger sample for homepage data

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,8 +2,8 @@ class StaticController < ApplicationController
   def homepage
     redirect_to dashboard_path() if logged_in?
 
-    @projects = Project.limit(100).sample(12).sort_by(&:name)
-    @users = User.limit(100).sample(45)
+    @projects = Project.limit(200).sample(14).sort_by(&:name)
+    @users = User.limit(200).sample(45)
     @quote = Quote.random
   end
 end
