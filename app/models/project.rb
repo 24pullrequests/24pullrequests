0ffc7617876056d03aa7d34c70dce77fc89e38a5
Andrew Nesbitt <andrewnez@gmail.com>
2012-11-30 19:15:24 +0000

    Added Assembly as a language

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
   validates_uniqueness_of :github_url, :message => "already been submitted"
   validates_length_of :description, :within => 20..200
 
-  LANGUAGES = ["ActionScript", "C", "C#", "C++", "Clojure", "CoffeeScript", "Common Lisp", "CSS", "Diff", 
+  LANGUAGES = ["ActionScript", "Assembly", "C", "C#", "C++", "Clojure", "CoffeeScript", "Common Lisp", "CSS", "Diff", 
                "Emacs Lisp", "Erlang", "Haskell", "HTML", "Java", "JavaScript", 
                "Lua", "Objective-C", "Perl", "PHP", "Python", "Ruby", "Scala", 
                "Scheme", "XML"]
