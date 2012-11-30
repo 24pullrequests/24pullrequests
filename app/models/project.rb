662f3d29d079a212797a1f2c4998a040176fba36
Andrew Nesbitt <andrewnez@gmail.com>
2012-11-30 18:33:01 +0000

    Avoid duplicate project submissions, fixes #40

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
   attr_accessible :description, :github_url, :name, :main_language
 
   validates_presence_of :description, :github_url, :name, :main_language
+  validates_uniqueness_of :github_url, :message => "already been submitted"
   validates_length_of :description, :within => 20..200
 
   LANGUAGES = ["ActionScript", "C", "C#", "C++", "Clojure", "CoffeeScript", "Common Lisp", "CSS", "Diff", 
