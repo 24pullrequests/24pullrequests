dc2e4c2fdc445edbb458a56084df882527137598
Andrew Nesbitt <andrewnez@gmail.com>
2012-11-30 10:32:04 +0000

    Added Clojure and CoffeeScript as languages

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,7 @@ class Project < ActiveRecord::Base
   validates_presence_of :description, :github_url, :name, :main_language
   validates_length_of :description, :within => 50..200
 
-  LANGUAGES = ["ActionScript", "C", "C#", "C++", "Common Lisp", "CSS", "Diff", 
+  LANGUAGES = ["ActionScript", "C", "C#", "C++", "Clojure", "CoffeeScript", "Common Lisp", "CSS", "Diff", 
                "Emacs Lisp", "Erlang", "Haskell", "HTML", "Java", "JavaScript", 
                "Lua", "Objective-C", "Perl", "PHP", "Python", "Ruby", "Scala", 
                "Scheme", "XML"]
