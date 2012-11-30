5476a5ee8efb4754a9deb16feaeff5e7d2f5b71f
Andrew Nesbitt <andrewnez@gmail.com>
2012-11-30 11:45:06 +0000

    Reduce minimum description length

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   attr_accessible :description, :github_url, :name, :main_language
 
   validates_presence_of :description, :github_url, :name, :main_language
-  validates_length_of :description, :within => 50..200
+  validates_length_of :description, :within => 20..200
 
   LANGUAGES = ["ActionScript", "C", "C#", "C++", "Clojure", "CoffeeScript", "Common Lisp", "CSS", "Diff", 
                "Emacs Lisp", "Erlang", "Haskell", "HTML", "Java", "JavaScript", 
