b7a721e2ed6cda0077c5919c323bc99a89a1cbea
Andrew Nesbitt <andrewnez@gmail.com>
2012-11-30 15:01:02 +0000

    Sort projects by name

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,7 +4,7 @@ class DashboardsController < ApplicationController
 
   def show
     pull_requests = current_user.pull_requests
-    projects = Project.limit(100).sample(12)
+    projects = Project.limit(100).sample(12).sort_by(&:name)
     render :show, :locals => { :user => current_user, :pull_requests => pull_requests, :projects => projects }
   end
 
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@ class StaticController < ApplicationController
   def homepage
     redirect_to dashboard_path() if logged_in?
 
-    @projects = Project.limit(100).sample(12)
+    @projects = Project.limit(100).sample(12).sort_by(&:name)
     @users = User.limit(100).sample(45)
     @quote = Quote.random
   end
