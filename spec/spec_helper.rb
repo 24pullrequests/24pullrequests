e95d4deb7953cd0f87f4a2a376624543d9236342
Eric J. Holmes <eric@ejholmes.net>
2012-11-30 23:00:30 -0800

    Add poltergeist for running js on travis. Local development still uses selenium.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,13 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+WebMock.disable_net_connect! allow_localhost: true
+
+if ENV['CI']
+  require 'capybara/poltergeist'
+  Capybara.javascript_driver = :poltergeist
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
