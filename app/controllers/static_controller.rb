20c8ce4600715b30518a1bae1c8cf9bb14dde50b
andrehjr <andrehjr@gmail.com>
2012-12-01 16:26:53 -0200

    List some pull requests at the homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,6 +4,7 @@ class StaticController < ApplicationController
 
     @projects = Project.limit(200).sample(14).sort_by(&:name)
     @users = User.limit(200).sample(45)
+    @pull_requests = PullRequest.limit(200).sample(5)
     @quote = Quote.random
   end
 end
