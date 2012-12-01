d260535f3e993d8085d10f3424fb3c00dc9ba26a
Eric J. Holmes <eric@ejholmes.net>
2012-12-01 15:46:23 -0800

    Add migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201194057) do
+ActiveRecord::Schema.define(:version => 20121201234440) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
