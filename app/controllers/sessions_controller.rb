be8ef30d8b3ade72aa9599804c81f89181f1617a
Eric J. Holmes <eric@ejholmes.net>
2012-12-01 10:36:43 -0800

    Green

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,8 @@ class SessionsController < ApplicationController
 
     session[:user_id] = user.id
     
-    if session[:pre_login_destination].nil?
-      redirect_to dashboard_path
-    end
-    
-    redirect_to session[:pre_login_destination]
+    redirect_to dashboard_path and return unless pre_login_destination
+    redirect_to pre_login_destination
   end
 
   def destroy
@@ -26,4 +23,11 @@ class SessionsController < ApplicationController
     flash[:notice] = params[:message]
     redirect_to root_path
   end
+
+private
+
+  def pre_login_destination
+    session[:pre_login_destination]
+  end
+
 end
