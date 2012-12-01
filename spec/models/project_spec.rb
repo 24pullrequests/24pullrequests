c80f4a66f5651498d379ef5e47374b09d878f64a
Eric J. Holmes <eric@ejholmes.net>
2012-12-01 15:42:18 -0800

    Add test.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -26,5 +26,18 @@ describe Project do
       it { should be_valid }
       its(:github_url) { should eq 'https://github.com/user/repo' }
     end
+
+    context 'when invalid' do
+      before do
+        project.github_url = 'not_a_repo'
+      end
+
+      it { should_not be_valid }
+      it 'raises an exception' do
+        expect { project.save! }.to raise_error ActiveRecord::RecordInvalid,
+          "Validation failed: Github url must be a github repository, e.g. " \
+          "'user/repo', or 'https://github.com/user/repo'"
+      end
+    end
   end
 end
