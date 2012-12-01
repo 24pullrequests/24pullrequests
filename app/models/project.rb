6837405309c1bf6b73eca8d2b2f7b7c4e935f249
Eric J. Holmes <eric@ejholmes.net>
2012-12-01 15:38:30 -0800

    DRY up

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,7 @@ class Project < ActiveRecord::Base
   validates_presence_of :description, :github_url, :name, :main_language
   validates_uniqueness_of :github_url, :message => "already been submitted"
   validates :github_url, :format => {
-    :with => /\Ahttps:\/\/github\.com\/[a-zA-Z_-].*\/[a-zA-Z_-].*\Z/,
+    :with => GithubUrl::REGEX,
     :message => "must be a github repository, e.g. 'user/repo', or 'https://github.com/user/repo'"
   }
   validates_length_of :description, :within => 20..200
--- lib/github_url.rb
+++ lib/github_url.rb
@@ -1,4 +1,5 @@
 module GithubUrl
+  REGEX = /\A(https?\:\/\/|git@)?(github\.com[:\/])?(?<username>[.1-9a-zA-Z_-]*)\/(?<repo>[.1-9a-zA-Z_-]*)/
 
   # Accepts the following formats:
   # https://github.com/user/repo
@@ -6,7 +7,7 @@ module GithubUrl
   # user/repo
   # git@github.com:user/repo.git
   def self.normalize(url)
-    m = /\A(https?\:\/\/|git@)?(github\.com[:\/])?(?<username>[.1-9a-zA-Z_-]*)\/(?<repo>[.1-9a-zA-Z_-]*)/.match(url)
+    m = REGEX.match(url)
     return url unless m
 
     "https://github.com/#{m[:username]}/#{m[:repo]}"
