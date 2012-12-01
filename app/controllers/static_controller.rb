92966aad27cbbd42dac28e245607bc8cfff64fc5
Andrew Nesbitt <andrewnez@gmail.com>
2012-12-01 11:00:22 +0000

    Removed empty brackets on the end of some routes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def homepage
-    redirect_to dashboard_path() if logged_in?
+    redirect_to dashboard_path if logged_in?
 
     @projects = Project.limit(200).sample(14).sort_by(&:name)
     @users = User.limit(200).sample(45)
