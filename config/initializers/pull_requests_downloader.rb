eb7de8dc887d2f01ea77d6f48a06757907148f8a
Eric J. Holmes <eric@ejholmes.net>
2012-11-30 18:53:35 -0800

    Remove feature envy from app/models/pull_requests_downloader.

--- config/initializers/pull_requests_downloader.rb
+++ config/initializers/pull_requests_downloader.rb
@@ -1,6 +1,6 @@
 Rails.application.config.pull_request_downloader = if Rails.env.production?
-                                                     ->(user) { PullRequestDownloader.new(user) }
+                                                     ->(login, oauth_token) { PullRequestDownloader.new(login, oauth_token) }
                                                    else
-                                                     ->(user) { Struct.new(:pull_requests).new([]) }
+                                                     ->(login, oauth_token) { Struct.new(:pull_requests).new([]) }
                                                    end
 
