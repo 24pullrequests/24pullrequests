6255e3af70137023bdafc1c798c252755a15361c
Eric J. Holmes <eric@ejholmes.net>
2012-11-30 20:24:45 -0800

    Spec for User model.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,4 +29,6 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+
+  config.include FactoryGirl::Syntax::Methods
 end
