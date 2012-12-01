02c13b16244db3f17e755e0e1830ffa5d3add4cb
Polarblau <polarblau@gmail.com>
2012-12-02 01:30:57 +0200

    Adding filter for suggested projects: * Adding request specs for filtering * Adding highlighting styles for reset and suggested projects filters * Adding filters links and helper * Adding specs for helpers * Updating JS functionality

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -19,4 +19,66 @@ describe 'Projects' do
       click_on 'Submit Project'
     end
   end
+
+  describe 'filtering the project list' do
+
+    context 'as anonymous user' do
+      before { visit projects_path }
+
+      it 'should not show the filter for user languages' do
+        within '#languages' do
+          page.should_not have_content 'Suggested for me'
+        end
+      end
+    end
+
+    context 'as logged-in user' do
+      before do
+        create :project, name: 'Ruby project', main_language: 'ruby'
+        create :project, name: 'Java project', main_language: 'java'
+        user.skills.create! language: 'Ruby'
+        login user
+        visit projects_path
+      end
+
+      it 'should show a filter for user languages' do
+        within '#languages' do
+          page.should have_content 'Suggested for you'
+        end
+      end
+
+      it 'should show both projects by default' do
+        within '#projects' do
+          page.should have_content 'Ruby project'
+          page.should have_content 'Java project'
+        end
+      end
+
+      it 'should filter projects in languages other than the selected' do
+        click_link 'ruby'
+        within '#projects' do
+          page.should have_css('.ruby', visible: true)
+          page.should have_css('.java', visible: false)
+        end
+      end
+
+      it 'should show only the Ruby project when clicking "Suggested for you"' do
+        click_link 'Suggested for you'
+        within '#projects' do
+          page.should have_css('.ruby', visible: true)
+          page.should have_css('.java', visible: false)
+        end
+      end
+
+      it 'should reset an active filter when clicking "Everything"' do
+        click_link 'ruby'
+        click_link 'Everything'
+        within '#projects' do
+          page.should have_css('.ruby', visible: true)
+          page.should have_css('.java', visible: true)
+        end
+      end
+
+    end
+  end
 end
