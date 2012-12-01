a50c94760b39773f0d2e9104f39c48ec5e6a8a21
Eric J. Holmes <eric@ejholmes.net>
2012-12-01 00:06:50 -0800

    home page spec.

--- spec/requests/homepage_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Homepage", js: true do
-  describe "GET /" do
-    it "Loads" do
-      visit '/'
-    end
-  end
-end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'Static pages' do
+  subject { page }
+
+  describe 'home page' do
+    before do
+      5.times do
+        create :user
+      end
+
+      visit root_path
+    end
+
+    it { should have_link('Log in with Github', href: login_path) }
+    it { should have_content('5 Developers already involved') }
+    it { should have_link('View All', href: users_path) }
+    it { should have_link('View All', href: projects_path) }
+    it { should have_link('Suggest a project', href: new_project_path) }
+  end
+end
