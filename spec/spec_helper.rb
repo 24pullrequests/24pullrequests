ada46118f94973eccb85a7a435bf6720b98258ee
Eric J. Holmes <eric@ejholmes.net>
2012-12-01 17:19:51 -0800

    Move mock_pull_request to a helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,22 +44,3 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
 end
-
-# Creates a mock pull request in json format.
-def mock_pull_request
-  {
-    'payload' => {
-      'pull_request' => {
-        'title'      => Faker::Lorem.words.first,
-        'issue_url'  => Faker::Internet.url,
-        'created_at' => DateTime.now.to_s,
-        'state'      => 'open',
-        'body'       => Faker::Lorem.paragraphs.join('\n'),
-        'merged'     => false
-      }
-    },
-    'repo' => {
-      'name' => Faker::Lorem.words.first
-    }
-  }
-end
