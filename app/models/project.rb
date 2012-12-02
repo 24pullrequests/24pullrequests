83928d9075ef563cfc5bb70e5cf45285f11798b4
Baris Balic <baris.balic@forward.co.uk>
2012-12-02 23:54:59 +0000

    Added rake task to clean URLs, changed the Project#github_url validations to allow only HTTP URLs, update the index to show Project#github_repository as it looks nicer.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,15 +2,16 @@ class Project < ActiveRecord::Base
   attr_accessible :description, :github_url, :name, :main_language
 
   validates_presence_of :description, :github_url, :name, :main_language
-  validates_uniqueness_of :github_url, :message => "already been submitted"
+  validates_format_of :github_url, :with => /^https?:\/\/(www\.)?github.com\/[\w-]*\/[\w-]*(\/)?$/i, :message => 'Enter the full HTTP URL.'
+  validates_uniqueness_of :github_url, :message => "Project has already been suggested."
   validates_length_of :description, :within => 20..200
 
   LANGUAGES = ["ActionScript", "Assembly", "C", "C#", "C++", "Clojure", "CoffeeScript",
                "ColdFusion", "CSS", "Emacs Lisp", "Erlang", "Haskell", "HTML", "Java", "JavaScript", 
                "Lua", "Objective-C", "Perl", "PHP", "PowerShell", "Python", "Ruby",
                "Scala", "Scheme", "Shell"]
-                         
-  def has_http_url?
-    (self.github_url =~ /^git@/).nil?
+
+  def github_repository
+    self.github_url.gsub(/^(((https|http|git)?:\/\/(www\.)?)|git@)github.com(:|\/)/i, '').gsub(/(\.git|\/)$/i, '')
   end
 end
