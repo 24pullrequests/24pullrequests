6edd78d725c5c1ff1011cd45350ae13ca85e3806
Eric J. Holmes <eric@ejholmes.net>
2012-12-01 17:19:51 -0800

    Move mock_pull_request to a helper.

---
+++ spec/support/pull_request_helpers.rb
@@ -0,0 +1,24 @@
+module PullRequestHelpers
+  # Creates a mock pull request in json format.
+  def mock_pull_request
+    {
+      'payload' => {
+        'pull_request' => {
+          'title'      => Faker::Lorem.words.first,
+          'issue_url'  => Faker::Internet.url,
+          'created_at' => DateTime.now.to_s,
+          'state'      => 'open',
+          'body'       => Faker::Lorem.paragraphs.join('\n'),
+          'merged'     => false
+        }
+      },
+      'repo' => {
+        'name' => Faker::Lorem.words.first
+      }
+    }
+  end
+end
+
+RSpec.configure do |config|
+  config.include PullRequestHelpers
+end
