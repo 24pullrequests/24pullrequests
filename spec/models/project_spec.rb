8835d664710329f01b041f4933f2125adb84a6dc
Baris Balic <baris.balic@forward.co.uk>
2012-12-03 00:12:45 +0000

    Fixed broken spec for Project after updating the github_urls.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -10,6 +10,6 @@ describe Project do
   it { should validate_presence_of(:github_url) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:main_language) }
-  it { should validate_uniqueness_of(:github_url).with_message('already been submitted') }
+  it { should validate_uniqueness_of(:github_url).with_message('Project has already been suggested.') }
   it { should ensure_length_of(:description).is_at_least(20).is_at_most(200) }
 end
