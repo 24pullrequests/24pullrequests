539337f5d701adff2223248ce6006a947144fecd
Kristján Pétursson <kristjan@gmail.com>
2012-12-05 01:08:15 -0800

    Easily switch on GitHub while developing
    It's hard to work with pull request data without having a real GitHub
    application set up. Now login and real pull request downloading are available in
    development if `ENV['GITHUB_KEY']` is present.

--- config/initializers/pull_requests_downloader.rb
+++ config/initializers/pull_requests_downloader.rb
@@ -1,4 +1,4 @@
-Rails.application.config.pull_request_downloader = if Rails.env.production?
+Rails.application.config.pull_request_downloader = if Rails.env.production? || !ENV['GITHUB_KEY'].blank?
                                                      ->(login, oauth_token) { PullRequestDownloader.new(login, oauth_token) }
                                                    else
                                                      ->(login, oauth_token) { Struct.new(:pull_requests).new([]) }
