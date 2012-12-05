81ece82a952dc470b1fa1e57678126844e60e17d
Eric J. Holmes <eric@ejholmes.net>
2012-12-04 20:06:40 -0800

    Localize.

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -2,7 +2,7 @@ class TwitterController < ApplicationController
   def authorize
     auth_hash = request.env['omniauth.auth']
     current_user.authorize_twitter!(auth_hash.credentials.token, auth_hash.credentials.secret)
-    flash[:notice] = "Your twitter account has been linked! We'll post a tweet whenever you open a pull request."
+    flash[:notice] = I18n.t 'twitter.account_linked'
     redirect_to dashboard_path
   end
 end
