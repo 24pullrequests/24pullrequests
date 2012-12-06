30fd1d0d69ce08d19dee6bf617d4a5de00525efa
Eric J. Holmes <eric@ejholmes.net>
2012-12-05 20:28:33 -0800

    tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,28 +4,48 @@ describe UsersController do
   let(:user) { create :user }
 
   describe 'GET index' do
-    before do
-      get :index
+    context 'as html' do
+      before do
+        get :index
+      end
+
+      it { should assign_to(:users).with(User.order('pull_requests_count desc').page(0)) }
     end
 
-    it { should assign_to(:users).with(User.order('pull_requests_count desc').page(0)) }
+    context 'as json' do
+      before do
+        get :index, format: :json
+      end
+
+      it { should respond_with_content_type(:json) }
+    end
   end
 
   describe 'GET show' do
-    context 'when the case matches' do
-      before do
-        get :show, id: user.nickname
+    context 'as html' do
+      context 'when the case matches' do
+        before do
+          get :show, id: user.nickname
+        end
+
+        it { should respond_with(200) }
       end
 
-      it { should respond_with(200) }
+      context 'when the case does not match' do
+        before do
+          get :show, id: user.nickname.upcase
+        end
+
+        it { should respond_with(200) }
+      end
     end
 
-    context 'when the case does not match' do
+    context 'as json' do
       before do
-        get :show, id: user.nickname.upcase
+        get :show, id: user.nickname, format: :json
       end
 
-      it { should respond_with(200) }
+      it { should respond_with_content_type(:json) }
     end
   end
 end
