5159dc19f3c19c4c2a97256af4fe9fec838b5072
Grégory Marcilhacy <g.marcilhacy@gmail.com>
2012-12-06 13:38:48 -0800

    enable memcache with Dalli in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,7 @@ Tfpullrequests::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.cache_store                       = :dalli_store
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
