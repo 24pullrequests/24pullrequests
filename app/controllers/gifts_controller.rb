45a66dfd0a99e3ba78625b12b478b279855a354f
Baris Balic <baris.balic@forward.co.uk>
2012-12-06 22:37:39 +0000

    Added User#unspent_pull_requests and updated GiftController#new to make use of it instead of standard pull requests.

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -9,7 +9,7 @@ class GiftsController < ApplicationController
 
   def new
     gift_form = GiftForm.new(:gift           => current_user.new_gift,
-                             :pull_requests  => current_user.pull_requests,
+                             :pull_requests  => current_user.unspent_pull_requests,
                              :giftable_dates => Gift.giftable_dates,
                              :date => params[:date])
 
