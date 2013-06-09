dba0efe08295bf6b6ebdf02de0e05ea4879069d1
Andrew Nesbitt <andrewnez@gmail.com>
2013-06-09 21:38:24 +0100

    Only run bugsnag in production

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,3 +1,5 @@
-Bugsnag.configure do |config|
-  config.api_key = "2dd469c5b922330293a39214672a53f8"
+if Rails.env.production?
+  Bugsnag.configure do |config|
+    config.api_key = "2dd469c5b922330293a39214672a53f8"
+  end
 end
