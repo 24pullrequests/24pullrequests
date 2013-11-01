f089f211f0d369221ebe0990b85b39acd5e134e3
Andrew Nesbitt <andrewnez@gmail.com>
2013-11-01 11:08:44 +0000

    Sendgrid config tweak

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,8 @@ Tfpullrequests::Application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
   config.action_mailer.delivery_method = :smtp
 end
