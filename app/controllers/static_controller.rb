711bd4ef3ef294b5eece31ee23cd31641cb2b228
Andrew Nesbitt <andrewnez@gmail.com>
2013-11-15 20:51:58 +0000

    Limit homepage pull requests to current year

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@ class StaticController < ApplicationController
   def homepage
     @projects = Project.limit(200).sample(14).sort_by(&:name)
     @users = User.order('pull_requests_count desc').limit(200).sample(45)
-    @pull_requests = PullRequest.order('created_at desc').limit(5)
+    @pull_requests = PullRequest.year(CURRENT_YEAR).order('created_at desc').limit(5)
     @quote = Quote.random
   end
 
