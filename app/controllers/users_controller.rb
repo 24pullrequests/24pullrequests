30d5d80b8edf3dbd800c194b83cc52a882799cd9
Andrew Nesbitt <andrewnez@gmail.com>
2013-11-15 11:59:30 +0000

    Allow viewing of calendars for different years

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
   end
 
   def show
+    @year      = params[:year].try(:to_i) || Time.now.year
     @user      = User.find_by_nickname!(params[:id])
-    @calendar  = Calendar.new(Gift.giftable_dates, @user.gifts)
+    @calendar  = Calendar.new(Gift.giftable_dates(@year), @user.gifts)
     respond_with @user
   end
 end
