faf13e1513759ef2937abe58ffb5a882ce6e4c09
Despo Pentara <despo@extractmethod.com>
2013-11-23 04:51:03 +0000

    add spec for gravatar url and remove lazy init

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,8 +15,7 @@ module ApplicationHelper
     link_to label, '#', :data => {:language => language}
   end
 
-  def gravatar_url(digest, size = '80')
-    digest ||= ''
+  def gravatar_url(digest='', size = '80')
     "https://secure.gravatar.com/avatar/#{digest}.png?s=#{size}&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"
   end
 
