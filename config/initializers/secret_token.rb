498e227b721cb92a03a8c3afe8aadba16bfb23f6
Peter Mellett <bulletraven@gmail.com>
2013-11-23 02:20:51 +0000

    rails 4 uses a different secret token hash name

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,5 +2,5 @@ if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
   raise 'SECRET_TOKEN environment variable must be set!'
 end
 
-Tfpullrequests::Application.config.secret_token =
+Tfpullrequests::Application.config.secret_key_base =
   ENV['SECRET_TOKEN'] || '9e54bbeeb2a8573aee35f49c56406f705443fc4afc9e7037a79b9cd8065b592df6d97b5f41ba12acc46fdf61a60363e00ea2debae684bad89cfd5a2b74049091'
