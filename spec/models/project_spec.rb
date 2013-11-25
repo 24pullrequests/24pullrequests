737fb68b41379a0935e39fbef602c192426d77b1
Despo Pentara <despo@despo.me>
2013-11-25 00:38:50 +0000

    add by_language scope to projects and users

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -8,22 +8,35 @@ describe Project do
   it { should validate_uniqueness_of(:github_url).with_message('Project has already been suggested.') }
   it { should ensure_length_of(:description).is_at_least(20).is_at_most(200) }
 
-  it "Validation should not pass on wrong programming language" do
-    project = FactoryGirl.build(:project)
-    project.main_language = "English"
-    project.valid?.should_not be_true
-    project.errors[:main_language].include?('must be a programming language').should be_true
+  context "validations" do
+    let(:project) { FactoryGirl.build(:project) }
+
+    it "should not pass on wrong programming language" do
+      project.main_language = "English"
+      project.valid?.should_not be_true
+      project.errors[:main_language].include?('must be a programming language').should be_true
+    end
+
+    it "should pass on correct programming language" do
+      project.main_language = "Ruby"
+      project.valid?.should be_true
+    end
+
+    it "should pass on github urls with periods" do
+      project.github_url = "https://github.com/kangax/fabric.js"
+      project.valid?.should be_true
+    end
   end
 
-  it "Validation should pass on correct programming language" do
-    project = FactoryGirl.build(:project)
-    project.main_language = "Ruby"
-    project.valid?.should be_true
-  end
-  
-  it "Validation should pass on github urls with periods" do
-    project = FactoryGirl.build(:project)
-    project.github_url = "https://github.com/kangax/fabric.js"
-    project.valid?.should be_true
+  context "#scopes" do
+    let!(:ruby_project) { FactoryGirl.create(:project, main_language: "Ruby") }
+
+    before do
+      ["Erlang", "JavaScript"].each { |lan| FactoryGirl.create(:project, main_language: lan)  }
+    end
+
+    it "by_language" do
+      Project.by_language("ruby").should eq([ruby_project])
+    end
   end
 end
