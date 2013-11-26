ec3c810414fe9ebbc8848ed6c397629bc7da8f56
Despo Pentara <despo@extractmethod.com>
2013-11-26 20:01:54 +0000

    pull requests for a language

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Tfpullrequests::Application.routes.draw do
   scope '/language/:language' do
     get '/', to: 'language#show', as: :language
     get '/projects', to: 'language#projects',  as: :language_projects
+    get '/pull_requests', to: 'language#pull_requests',  as: :language_pull_requests
     get '/users', to: 'language#users',  as: :language_users
   end
 
