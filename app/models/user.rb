88702a11df28323016c210fc0a4a486361dd210d
Andrew Nesbitt <andrew.nesbitt@github.com>
2013-12-01 20:12:09 +0000

    Removed unused variable

--- app/models/user.rb
+++ app/models/user.rb
@@ -171,7 +171,7 @@ class User < ActiveRecord::Base
   end
 
   def twitter
-    @twitter ||= client = Twitter::REST::Client.new do |config|
+    @twitter ||= Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_KEY']
       config.consumer_secret     = ENV['TWITTER_SECRET']
       config.access_token        = twitter_token
