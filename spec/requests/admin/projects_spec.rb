3b612c030eb4571142691ad436aa2d4b5da07efd
Despo Pentara <despo@despo.me>
2013-12-01 20:22:10 +0000

    change /admin/projects spec description so its more obvious what fails

--- spec/requests/admin/projects_spec.rb
+++ spec/requests/admin/projects_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'Projects' do
+describe 'Admin Projects' do
   let(:user) { create :user }
   let!(:projects) { [ "repo1", "repo2"].map { |repo| create :project, github_url: "http://github.com/christmas/#{repo}" } }
   subject { page }
@@ -27,7 +27,7 @@ describe 'Projects' do
       click_on "Search"
 
       sleep(Capybara.default_wait_time)
-      
+
       should have_content "repo1"
       should_not have_content "repo2"
     end
