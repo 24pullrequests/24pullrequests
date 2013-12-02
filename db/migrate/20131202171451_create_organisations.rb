7f0d27b50c638c3b094c6d9ac680ec802dfed8cf
Baris Balic <baris.balic@uswitch.com>
2013-12-02 17:30:33 +0000

    Added model object, after_create hook and updating the user view and json accordingly.

---
+++ db/migrate/20131202171451_create_organisations.rb
@@ -0,0 +1,18 @@
+class CreateOrganisations < ActiveRecord::Migration
+  def change
+    create_table :organisations do |t|
+      t.string :login
+      t.string :avatar_url
+      t.integer :github_id
+
+      t.timestamps
+    end
+
+    create_table :organisations_users do |t|
+      t.integer :user_id
+      t.integer :organisation_id
+    end
+
+    add_index :organisations, [:login], :unique => true
+  end
+end
