015fbef3c814da559c25ba0315bc0b469832b26d
Baris Balic <baris.balic@uswitch.com>
2013-12-02 17:30:33 +0000

    Added model object, after_create hook and updating the user view and json accordingly.

--- config/initializers/pull_requests_downloader.rb
+++ config/initializers/pull_requests_downloader.rb
@@ -1,6 +1,6 @@
 Rails.application.config.pull_request_downloader = if Rails.env.production? || !ENV['GITHUB_KEY'].blank?
                                                      ->(login, oauth_token) { PullRequestDownloader.new(login, oauth_token) }
                                                    else
-                                                     ->(login, oauth_token) { Struct.new(:pull_requests).new([]) }
+                                                     ->(login, oauth_token) { Struct.new(:pull_requests, :user_organisations).new([], []) }
                                                    end
 
