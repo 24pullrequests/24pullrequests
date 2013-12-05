10a084d7d71b7e2a3646fa75d8bbe45c1398f63b
Joah Gerstenberg <joahd@gbergfamily.com>
2013-12-04 22:24:08 -0600

    Updated to fix bug noticed by Travis CI

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -28,7 +28,7 @@ class GiftsController < ApplicationController
 
   def edit
     gift_form = GiftForm.new(:gift => gift,
-                             :pull_requests => current_user.unspent_pull_requests
+                             :pull_requests => current_user.unspent_pull_requests)
 
     render :new, :locals => { :gift_form => gift_form }
   end
