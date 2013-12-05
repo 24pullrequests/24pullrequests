caf964ea3e91b37dd08d6348010ec58a146ff28b
Joah Gerstenberg <joahd@gbergfamily.com>
2013-12-05 10:08:15 -0600

    Wrong syntax error XD

--- spec/requests/gifts_spec.rb
+++ spec/requests/gifts_spec.rb
@@ -7,13 +7,13 @@ describe 'Gifts' do
 	describe '#new'
 		before do
 			let!(:pull_request) { create :pull_request, user: user }
-			let!(:gift) 		{ create(:gift, user: user, pull_request: pull_request)
+			let!(:gift) 		{ create(:gift, user: user, pull_request: pull_request) }
 		end
 
-		should_not have_xpath "//option[contains(text(), '#{pull_request.title}'}]"
+		should_not have_xpath "//option[contains(text(), '#{pull_request.title}')]"
 
 		click_on "Gift it!"
 
-		should_not have_xpath "//option[contains(text(), '#{pull_request.title}'}]"
+		should_not have_xpath "//option[contains(text(), '#{pull_request.title}')]"
 	end
 end
