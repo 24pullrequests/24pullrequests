1aa0ec4e29a9d4241d5185e8c4f781ed18f0ab7a
Joah Gerstenberg <joahd@gbergfamily.com>
2013-12-05 10:53:57 -0600

    So I just learned not to put a let() statement in a before block...

--- spec/requests/gifts_spec.rb
+++ spec/requests/gifts_spec.rb
@@ -2,14 +2,11 @@ require 'spec_helper'
 
 describe 'Gifts' do
 	subject { page }
-	let(:user) { create :user, nickname: "akira" }
+	let(:user)          { create :user, nickname: "akira" }
+	let(:pull_request)  { create(:pull_request, user: user, title: 'Example Pull Request') }
+	let(:gift)          { create(:gift, user: user, pull_request: pull_request) }
 
 	describe '#new' do
-		before do
-			let(:pull_request) { create(:pull_request, user: user, title: 'Example Pull Request') }
-			let(:gift) 		{ create(:gift, user: user, pull_request: pull_request) }
-		end
-
 		should_not have_xpath "//option[contains(text(), '#{pull_request.title}')]"
 
 		click_on "Gift it!"
