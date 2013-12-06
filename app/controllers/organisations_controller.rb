d82b9958d19a61b73ea482379f591d55733eb0bb
Baris Balic <baris.balic@uswitch.com>
2013-12-06 01:08:47 +0000

    Revert "Merge pull request #472 from JoahG/organisations-fix"
    This reverts commit 206f15978dee96945de96912e705da7b6daef2d2, reversing
    changes made to 453307460310d8847146ef89d2fd24bac8cc2d4d.

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -4,7 +4,7 @@ class OrganisationsController < ApplicationController
   respond_to :js, only: :index
 
   def index
-    @organisations = Organisation.order('pull_requests_count desc').includes(:users).page params[:page]
+    @organisations = Organisation.includes(:users).page params[:page]
     respond_with @organisations
   end
 
