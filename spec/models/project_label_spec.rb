7665f6456501e979caa579489759fc3a5ed2480c
Despo Pentara <despo@despo.me>
2013-12-11 00:46:48 +0000

    ensure a label can only be assigned to a project once

--- spec/models/project_label_spec.rb
+++ spec/models/project_label_spec.rb
@@ -5,8 +5,11 @@ describe ProjectLabel do
 
   context "validations" do
     it "must have a label" do
-      should have(1).error_on(:label)
+      should validate_presence_of(:label_id)
     end
 
+    it "must only have a label allocated to a project once" do
+      should validate_uniqueness_of(:label_id).scoped_to(:project_id)
+    end
   end
 end
