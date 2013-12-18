813e455cc740626838672535a583f2f52de43823
Ashish Dixit <tundal45@gmail.com>
2013-12-18 10:46:28 -0600

    Provide the option to gift pull requests only when there are unspent pull requests available.

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -20,13 +20,37 @@ describe 'Dashboard' do
     before do
       user.email_frequency = 'daily'
       user.save!
-      visit dashboard_path
     end
 
-    it { should have_content "Welcome back, #{user.nickname}" }
+    it do
+      visit dashboard_path
+      should have_content "Welcome back, #{user.nickname}"
+    end
 
     context 'when the user has not sent any pull requests' do
+      before do
+        visit dashboard_path
+      end
+
       it { should have_content "You've not sent any pull requests, what are you waiting for?!" }
+    end
+
+    context 'when the user has unspent pull requests' do
+      before do
+        user.pull_requests.create(attributes_for(:pull_request))
+        visit dashboard_path
+      end
+
+      it { should have_content "Looks like you haven't gifted any code today. Would you like to gift your new pull requests?" }
+      it { should have_button "Gift it!" }
+    end
+
+    context 'when the user does not have any unspent pull requests' do
+      before do
+        visit dashboard_path
+      end
+
+      it { should_not have_button "Gift it!" }
     end
   end
 
