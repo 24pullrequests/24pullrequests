87380f3f9b02ac7e0829b935b4007adbdff89e8d
Adam Butler <adam@littlebigmedia.co.uk>
2014-10-24 12:32:21 +0100

    Add time of event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
   belongs_to :user
 
   def formatted_date
-    self.start_time.strftime('%A %d %B %Y')
+    self.start_time.strftime('%A %d %B %Y at %I:%M%p')
   end
 
   def current_user_is_owner(current_user)
