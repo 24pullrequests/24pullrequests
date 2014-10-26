e255270c3d01ad6e845bb94e469f36b39c462ff9
Adam Butler <adam@littlebigmedia.co.uk>
2014-10-26 15:50:55 +0000

    Event form validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ActiveRecord::Base
 
   belongs_to :user
+  validates_presence_of :organiser, :location, :url, :start_time, :latitude, :longitude, :description
+  validates :url, :format => URI::regexp(%w(http https))
 
   def formatted_date
     self.start_time.strftime('%A %d %B %Y at %I:%M%p')
