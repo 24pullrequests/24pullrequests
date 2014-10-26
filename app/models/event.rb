bae1038fa558559fa481b03ee3944b6429c33851
Adam Butler <adam@littlebigmedia.co.uk>
2014-10-26 16:36:56 +0000

    Fix hound reported issues

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,16 +1,15 @@
 class Event < ActiveRecord::Base
-
   belongs_to :user
   validates_presence_of :organiser, :location, :url, :start_time, :latitude, :longitude, :description
-  validates :url, :format => URI::regexp(%w(http https))
+  validates :url, format: URI::regexp(%w(http https))
   validate :validate_start_time
 
   def formatted_date
-    self.start_time.strftime('%A %d %B %Y at %I:%M%p')
+    start_time.strftime('%A %d %B %Y at %I:%M%p')
   end
 
   def current_user_is_owner(current_user)
-    self.user == current_user
+    user == current_user
   end
 
   def validate_start_time
@@ -18,5 +17,4 @@ class Event < ActiveRecord::Base
       errors.add(:start_time, :not_in_range)
     end
   end
-
 end
