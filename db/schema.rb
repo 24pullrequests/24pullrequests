97e024ebf6f1c3178947042394a37e615a1f4353
Adam Butler <adam@littlebigmedia.co.uk>
2014-10-27 21:55:52 +0000

    organiser -> name in events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027181959) do
+ActiveRecord::Schema.define(version: 20141027215446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20141027181959) do
   end
 
   create_table "events", force: true do |t|
-    t.string   "organiser"
+    t.string   "name"
     t.string   "location"
     t.string   "url"
     t.datetime "start_time"
