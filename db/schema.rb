0e7d870baa2eac413b305c5087ab94a098c0bc62
Adam Butler <adam@littlebigmedia.co.uk>
2014-10-27 18:21:08 +0000

    Add index to events start_time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026172558) do
+ActiveRecord::Schema.define(version: 20141027181959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20141026172558) do
     t.text     "description"
     t.integer  "user_id"
   end
+
+  add_index "events", ["start_time"], name: "index_events_on_start_time", using: :btree
 
   create_table "gifts", force: true do |t|
     t.integer  "user_id",         null: false
