9f91341de68906d5be03b098d3dac4adbf6cd57b
Adam Butler <adam@littlebigmedia.co.uk>
2014-10-28 12:39:49 +0000

    Add spec to validate that start_time is not in the past

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ActiveRecord::Base
   validates_presence_of :name, :location, :url, :start_time, :latitude, :longitude, :description
   validates :url, format: URI::regexp(%w(http https))
   validate :validate_start_time_is_in_range, if: :start_time
+  validate :validate_start_time_is_not_in_the_past, if: :start_time
 
   def formatted_date
     start_time.strftime("%A %d %B %Y at %I:%M%p")
@@ -16,6 +17,12 @@ class Event < ActiveRecord::Base
   def validate_start_time_is_in_range
     unless (Time.parse("1st December")..Time.parse("24th December")).cover?(start_time)
       errors.add(:start_time, :not_in_range)
+    end
+  end
+
+  def validate_start_time_is_not_in_the_past
+    unless start_time >= Time.now.beginning_of_day
+      errors.add(:start_time, :in_past)
     end
   end
 end
