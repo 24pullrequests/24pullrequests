c4453d290ffc6985f8fda70b7e0bd89c7f74372f
Adam Butler <adam@littlebigmedia.co.uk>
2014-10-28 08:41:07 +0000

    Further start_time validations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -12,16 +12,37 @@ describe Event, type: :model do
   context "validations" do
     let(:event) { FactoryGirl.build(:event) }
 
-    it "should not pass with a date outside of 1st December - 24th December" do
-      event.start_time = Time.parse("30th November")
-      expect(event.valid?).not_to be true
-      expect(event.errors[:start_time].include?("Events can only be created between 1st and 24th December")).to be true
+    it "should not pass with a start_at outside of 1st December - 24th December" do
+      Timecop.freeze(Time.parse("1st January 1970")) do
+        event.start_time = Time.parse("30th November")
+        expect(event.valid?).not_to be true
+        expect(event.errors[:start_time].include?("Events can only be created between 1st and 24th December")).to be true
+      end
     end
 
-    it "should not pass with a date within of 1st December - 24th December" do
-      ["1st December", "15th December", "24th December"].each do |time|
-        event.start_time = Time.parse(time)
+    it "should not pass with a start_at in the past" do
+      Timecop.freeze(Time.parse("15th December 2014")) do
+        event.start_time = Time.parse("14th December 2014")
+        expect(event.valid?).not_to be true
+        expect(event.errors[:start_time].include?("Events can only be created in the past")).to be true
+      end
+    end
+
+    it "should pass with a start_at on current date" do
+      Timecop.freeze(Time.parse("15th December 2014 12:00")) do
+        event.start_time = Time.parse("15th December 2014 13:00")
         expect(event.valid?).to be true
+      end
+    end
+
+    context "after 1st December" do
+      it "should not pass with a start_at within of 1st December - 24th December" do
+        Timecop.freeze(Time.parse("1st December 2014")) do
+          ["1st December", "15th December", "24th December"].each do |time|
+            event.start_time = Time.parse(time)
+            expect(event.valid?).to be true
+          end
+        end
       end
     end
   end
