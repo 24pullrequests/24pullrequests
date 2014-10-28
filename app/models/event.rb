1e1276253fb51604bc8c384223ef492542b6cace
Adam Butler <adam@littlebigmedia.co.uk>
2014-10-28 12:39:01 +0000

    More explicit name for validate_start_time_is_in_range validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :name, :location, :url, :start_time, :latitude, :longitude, :description
   validates :url, format: URI::regexp(%w(http https))
-  validate :validate_start_time
+  validate :validate_start_time_is_in_range, if: :start_time
 
   def formatted_date
     start_time.strftime("%A %d %B %Y at %I:%M%p")
@@ -13,7 +13,7 @@ class Event < ActiveRecord::Base
     user_id == user.id or user.is_admin?
   end
 
-  def validate_start_time
+  def validate_start_time_is_in_range
     unless (Time.parse("1st December")..Time.parse("24th December")).cover?(start_time)
       errors.add(:start_time, :not_in_range)
     end
