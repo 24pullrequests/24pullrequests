dd1ca302a402443ab5936b3b46ee863dfa87240a
Mu-An Chiou <me@muanchiou.com>
2014-11-06 13:15:29 +0800

    Clean up avatar list

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,8 @@
 class StaticController < ApplicationController
   def homepage
     @projects = Project.includes(:labels).active.limit(200).sample(14)
-    @users = User.users_with_pull_request_counts(current_year).limit(200).sample(45)
-    @orgs = Organisation.with_user_counts.order_by_pull_requests.limit(200).sample(45)
+    @users = User.users_with_pull_request_counts(current_year).limit(200).sample(40)
+    @orgs = Organisation.with_user_counts.order_by_pull_requests.limit(200).sample(40)
     @pull_requests = PullRequest.year(current_year).order('created_at desc').limit(5)
   end
 
