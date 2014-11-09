ebad3efaa110e2747cef17d5cb95156297b38046
Andrew Nesbitt <andrewnez@gmail.com>
2014-11-09 19:18:21 +0000

    Moved user avatars to use the new github avatar endpoint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,10 +21,6 @@ module ApplicationHelper
     link_to label, '#', data: {language: language}
   end
 
-  def gravatar_url(digest='', size = '80')
-    "https://secure.gravatar.com/avatar/#{digest}.png?s=#{size}&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"
-  end
-
   def github_button(nickname)
     %(<iframe src="http://ghbtns.com/github-btn.html?user=#{nickname}&type=follow&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="300" height="20"></iframe>).html_safe
   end
