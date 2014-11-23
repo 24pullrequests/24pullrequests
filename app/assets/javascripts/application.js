95cbbfbe0e7502e2c32118fce89e9b86a1e8aa72
Andrew Nesbitt <andrewnez@gmail.com>
2014-11-23 23:21:16 +0000

    Switched from bootstrap-rails to bootstrap-sass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+//= require bootstrap-sprockets
 //= require typeahead.min
 //= require coderwall
 //= require jquery.timeago
