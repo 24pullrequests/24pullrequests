164476aa40b5865c4cd5f107e3590d2be78353cb
Mu-An Chiou <me@muanchiou.com>
2014-11-29 17:13:33 -0800

    Clean up homepage - bottom half

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,8 @@
 class StaticController < ApplicationController
   def homepage
     @projects = Project.includes(:labels).active.limit(200).sample(14)
-    @users = User.order('pull_requests_count desc').includes(:pull_requests).limit(200).sample(40)
-    @orgs = Organisation.with_user_counts.order_by_pull_requests.limit(200).sample(40)
+    @users = User.order('pull_requests_count desc').includes(:pull_requests).limit(200).sample(24)
+    @orgs = Organisation.with_user_counts.order_by_pull_requests.limit(200).sample(24)
     @pull_requests = PullRequest.year(current_year).order('created_at desc').limit(5)
 
     render layout: "homepage"
