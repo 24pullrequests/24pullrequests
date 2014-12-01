f0819ecec82fd3c154389f520e3b0a3484e7fc8a
John Beynon <johnb@heroku.com>
2014-12-01 11:46:33 +0000

    Update to use CURRENT_YEAR

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   respond_to :js, only: :index
 
   def index
-    @users = User.order('pull_requests_count desc').where('EXTRACT(year from "created_at") = ?', Date.today.year).includes(:pull_requests).page params[:page]
+    @users = User.order('pull_requests_count desc').where('EXTRACT(year from "created_at") = ?', CURRENT_YEAR).includes(:pull_requests).page params[:page]
     respond_with @users
   end
 
