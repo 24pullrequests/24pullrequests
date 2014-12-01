1118552e542c1394847a296b8a2e95ed1b6ef5f1
Kyle Daigle <kyle.daigle@github.com>
2014-12-01 10:26:04 -0500

    Add a way to score a project

---
+++ spec/services/popularity_scorer_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+describe PopularityScorer do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:project) { FactoryGirl.create(:project) }
+  subject(:popularity_scorer) { PopularityScorer.new(user, project) }
+
+  describe "#score" do
+    it "returns an integer score of project popularity" do
+      expect(project).to receive(:commits).and_return([])
+      expect(project).to receive(:issues).and_return([])
+      expect(popularity_scorer.score).to be(0)
+    end
+
+    it "returns a score of 5 if there are 50 recent commits" do
+      commit = double(:commit)
+      expect(project).to receive(:commits).and_return([commit] * 50)
+      expect(project).to receive(:issues).and_return([])
+      expect(popularity_scorer.score).to eq(5)
+    end
+
+    it "returns a score of 3 if there are 5 recent issues" do
+      issue = double(:issue)
+      expect(project).to receive(:commits).and_return([])
+      expect(project).to receive(:issues).and_return([issue] * 5)
+      expect(popularity_scorer.score).to eq(3)
+    end
+
+    it "does not give more than the maximum points" do
+      commit = double(:commit)
+      expect(project).to receive(:commits).and_return([commit] * 200)
+      expect(project).to receive(:issues).and_return([])
+      expect(popularity_scorer.score).to eq(10)
+    end
+  end
+
+end
