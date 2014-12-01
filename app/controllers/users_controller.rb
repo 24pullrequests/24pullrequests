c802def24d86d8a529d8a88b1000976f240f1607
John Beynon <johnb@heroku.com>
2014-12-01 12:21:47 +0000

    Scope created_at correctly and use JOINS instead of INCLUDES to query joined table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   respond_to :js, only: :index
 
   def index
-    @users = User.order('pull_requests_count desc').where('EXTRACT(year from "created_at") = ?', CURRENT_YEAR).includes(:pull_requests).page params[:page]
+    @users = User.order('pull_requests_count desc').joins(:pull_requests).where('EXTRACT(year from pull_requests.created_at) = ?', CURRENT_YEAR).page params[:page]
     respond_with @users
   end
 
