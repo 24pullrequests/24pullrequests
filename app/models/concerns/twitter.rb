2ea06c6ebc07bc60ea95e32a82e27fa6d32ddca8
TheSooth <TheSooth@users.noreply.github.com>
2014-12-05 03:20:15 +0200

    Update twitter.rb

--- app/models/concerns/twitter.rb
+++ app/models/concerns/twitter.rb
@@ -8,7 +8,7 @@ module Concerns
 
     module InstanceMethods
       def twitter
-        @twitter ||= ::Twitter::Client.new do |config|
+        @twitter ||= ::Twitter::REST::Client.new do |config|
           config.consumer_key        = ENV['TWITTER_KEY']
           config.consumer_secret     = ENV['TWITTER_SECRET']
           config.access_token        = twitter_token
