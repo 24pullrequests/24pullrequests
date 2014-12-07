7f57ba1b11246df544bc41faf77f3d0c06297b42
Andrew Nesbitt <andrewnez@gmail.com>
2014-12-07 09:39:59 +0000

    Didn’t mean to commit that

--- config/initializers/humans.rb
+++ config/initializers/humans.rb
@@ -5,12 +5,12 @@ module Humans
         # Memoize at boot up to prevent GitHub's rate limiting. This should work
         # fine for now.
         access_token = User.limit(1).order("RANDOM()").pluck(:token).first
-        Rails.application.config.octokit_client = Octokit::Client.new(access_token: access_token,
+        config.octokit_client = Octokit::Client.new(access_token: access_token,
                                                     auto_paginate: true)
 
         config.contributors = begin
           Timeout::timeout(5) {
-            Rails.application.config.octokit_client.contributors('24pullrequests/24pullrequests')
+            config.octokit_client.contributors('24pullrequests/24pullrequests')
           }
         rescue => e
           puts "Error when memoizing contributors at boot up:\n #{e.inspect}"
