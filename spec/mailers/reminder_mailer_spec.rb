40de6ae643413ccb5a63118b7597511092857dab
Peter Souter <p.souter@kainos.com>
2014-12-09 20:09:47 +0000

    An excellent refactor provided by @tokland
    Original SO post here: http://codereview.stackexchange.com/questions/72161/dry-ing-up-some-rspec/72168#72168

--- spec/mailers/reminder_mailer_spec.rb
+++ spec/mailers/reminder_mailer_spec.rb
@@ -1,53 +1,56 @@
 require 'spec_helper'
 
 describe ReminderMailer, type: :mailer do
+  let(:user) do
+    mock_model(User,
+    {
+      nickname: 'David',
+      email: 'david@example.com',
+      languages: ['Ruby'],
+      skills: [],
+      pull_requests: double(:pull_request, year: []),
+      suggested_projects: [],
+    })
+  end
 
-  ['daily','weekly'].each do |time_format|
-    describe time_format do
-      let(:time_format) { time_format }
-      let(:user) do
-        mock_model(User, nickname:           'David',
-                         email:              'david@example.com',
-                         languages:          ['Ruby'],
-                         skills:             [],
-                         pull_requests:      double(:pull_request, year: []),
-                         suggested_projects: [])
-      end
-          let(:mail) {
-            if time_format == 'daily'
-              ReminderMailer.daily(user)
-            else
-              ReminderMailer.weekly(user)
-            end
-          }
+  shared_examples :reminder_mailer do |subject:, this_time:, next_time:|
+    it 'renders the subject' do
+      expect(mail.subject).to eq(subject)
+    end
 
-      it 'renders the subject' do
-        expect(mail.subject).to eq("[24 Pull Requests] #{time_format.capitalize} Reminder")
-      end
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
 
-      it 'renders the receiver email' do
-        expect(mail.to).to eq([user.email])
-      end
+    it 'renders the sender email' do
+      expect(mail['From'].to_s).to eq('24 Pull Requests <info@24pullrequests.com>')
+    end
 
-      it 'renders the sender email' do
-        expect(mail['From'].to_s).to eq('24 Pull Requests <info@24pullrequests.com>')
-      end
+    it 'uses nickname' do
+      expect(mail.body.encoded).to match(user.nickname)
+    end
 
-      it 'uses nickname' do
-        expect(mail.body.encoded).to match(user.nickname)
-      end
-
-      it "says #{time_format}" do
-        if time_format == 'weekly'
-          expect(mail.body.encoded).to match("We've got some suggested projects for you to send pull requests to this week")
-          expect(mail.body.encoded).to match("See you next week!")
-        elsif time_format == 'daily'
-          expect(mail.body.encoded).to match("We've got some suggested projects for you to send pull requests to today")
-          expect(mail.body.encoded).to match("See you tomorrow!")
-        end
-      end
-
+    it 'contains periodicity in body' do
+      expect(mail.body.encoded).to match("We've got some suggested projects for you to send pull requests to #{this_time}")
+      expect(mail.body.encoded).to match("See you #{next_time}!")
     end
   end
 
+  describe 'daily' do
+    let(:mail) { ReminderMailer.daily(user) }
+
+    it_behaves_like :reminder_mailer,
+      subject: '[24 Pull Requests] Daily Reminder',
+      this_time: 'today',
+      next_time: 'tomorrow'
+  end
+
+  describe 'weekly' do
+    let(:mail) { ReminderMailer.weekly(user) }
+
+    it_behaves_like :reminder_mailer,
+      subject: '[24 Pull Requests] Weekly Reminder',
+      this_time: 'week',
+      next_time: 'next week'
+  end
 end
