1f460eeca7a5e229a8f2420d7e63d9f88d9c953f
Peter Souter <p.souter@kainos.com>
2014-12-09 17:16:38 +0000

    DRY up the reminder mail spec
    Use an array to half the amount of specs we need to write :+1:
    
    Also add a more explicit check of the email contents to check our new time format changes

--- spec/mailers/reminder_mailer_spec.rb
+++ spec/mailers/reminder_mailer_spec.rb
@@ -2,70 +2,52 @@ require 'spec_helper'
 
 describe ReminderMailer, type: :mailer do
 
-  describe 'daily' do
-    let(:user) do
-      mock_model(User, nickname:           'David',
-                       email:              'david@example.com',
-                       languages:          ['Ruby'],
-                       skills:             [],
-                       pull_requests:      double(:pull_request, year: []),
-                       suggested_projects: [])
+  ['daily','weekly'].each do |time_format|
+    describe time_format do
+      let(:time_format) { time_format }
+      let(:user) do
+        mock_model(User, nickname:           'David',
+                         email:              'david@example.com',
+                         languages:          ['Ruby'],
+                         skills:             [],
+                         pull_requests:      double(:pull_request, year: []),
+                         suggested_projects: [])
+      end
+          let(:mail) {
+            if time_format == 'daily'
+              ReminderMailer.daily(user)
+            else
+              ReminderMailer.weekly(user)
+            end
+          }
+
+      it 'renders the subject' do
+        expect(mail.subject).to eq("[24 Pull Requests] #{time_format.capitalize} Reminder")
+      end
+
+      it 'renders the receiver email' do
+        expect(mail.to).to eq([user.email])
+      end
+
+      it 'renders the sender email' do
+        expect(mail['From'].to_s).to eq('24 Pull Requests <info@24pullrequests.com>')
+      end
+
+      it 'uses nickname' do
+        expect(mail.body.encoded).to match(user.nickname)
+      end
+
+      it "says #{time_format}" do
+        if time_format == 'weekly'
+          expect(mail.body.encoded).to match("We've got some suggested projects for you to send pull requests to this week")
+          expect(mail.body.encoded).to match("See you next week!")
+        elsif time_format == 'daily'
+          expect(mail.body.encoded).to match("We've got some suggested projects for you to send pull requests to today")
+          expect(mail.body.encoded).to match("See you tomorrow!")
+        end
+      end
+
     end
-    let(:mail) { ReminderMailer.daily(user) }
-
-    it 'renders the subject' do
-      expect(mail.subject).to eq('[24 Pull Requests] Daily Reminder')
-    end
-
-    it 'renders the receiver email' do
-      expect(mail.to).to eq([user.email])
-    end
-
-    it 'renders the sender email' do
-      expect(mail['From'].to_s).to eq('24 Pull Requests <info@24pullrequests.com>')
-    end
-
-    it 'uses nickname' do
-      expect(mail.body.encoded).to match(user.nickname)
-    end
-
-    it 'says daily' do
-      expect(mail.body.encoded).to match('today')
-    end
-
-  end
-
-  describe 'weekly' do
-    let(:user) do
-      mock_model(User, nickname:           'David',
-                       email:              'david@example.com',
-                       languages:          ['Ruby'],
-                       skills:             [],
-                       pull_requests:      double(:pull_request, year: []),
-                       suggested_projects: [])
-    end
-    let(:mail) { ReminderMailer.weekly(user) }
-
-    it 'renders the subject' do
-      expect(mail.subject).to eq('[24 Pull Requests] Weekly Reminder')
-    end
-
-    it 'renders the receiver email' do
-      expect(mail.to).to eq([user.email])
-    end
-
-    it 'renders the sender email' do
-      expect(mail['From'].to_s).to eq('24 Pull Requests <info@24pullrequests.com>')
-    end
-
-    it 'uses nickname' do
-      expect(mail.body.encoded).to match(user.nickname)
-    end
-
-    it 'says weekly' do
-      expect(mail.body.encoded).to match('week')
-    end
-
   end
 
 end
