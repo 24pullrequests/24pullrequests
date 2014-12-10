7ead184a82929519033191af2ef9acce90fac024
Andrew Nesbitt <andrewnez@gmail.com>
2014-12-10 19:17:48 +0000

    Added preview for daily email

--- test/mailers/previews/reminder_mailer_preview.rb
+++ test/mailers/previews/reminder_mailer_preview.rb
@@ -1,6 +1,13 @@
 class ReminderMailerPreview < ActionMailer::Preview
+  # http://localhost:3000/rails/mailers/reminder_mailer/november
   def november
     user = User.first
     ReminderMailer.november(user)
+  end
+
+  # http://localhost:3000/rails/mailers/reminder_mailer/daily
+  def daily
+    user = User.first
+    ReminderMailer.daily(user)
   end
 end
