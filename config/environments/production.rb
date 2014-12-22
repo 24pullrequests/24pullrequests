b7fa4554b084890f3d73069606f60d6b793d039e
Arthur Neves <arthurnn@gmail.com>
2014-12-21 20:30:42 -0500

    serve assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Tfpullrequests::Application.configure do
   config.cache_store                       = :dalli_store
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_files = false
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
