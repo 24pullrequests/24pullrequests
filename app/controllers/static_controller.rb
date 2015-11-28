d42de4e358e6a86d8925f3f893b572dc50bf3e5a
Peter Mellett <pjm@petermellett.co.uk>
2015-11-28 18:15:51 +0000

    move contributor map to static about page, above the list of contributors

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -8,6 +8,15 @@ class StaticController < ApplicationController
 
   def about
     @contributors = User.contributors
+
+    # a user's location is pulled from public GitHub data
+    # available on their profile when authenticating with the site
+    @map_markers = Gmaps4rails.build_markers(active_users) do |user, marker|
+      next if user.lat.nil? || user.lng.nil?
+
+      marker.lat user.lat
+      marker.lng user.lng
+    end
   end
 
   def sponsors
@@ -16,5 +25,11 @@ class StaticController < ApplicationController
 
   def humans
     @contributors = User.contributors
+  end
+
+protected
+  # Currently-active contributors, i.e. users with a pull request this year
+  def active_users
+    PullRequest.year(current_year).map(&:user).uniq
   end
 end
