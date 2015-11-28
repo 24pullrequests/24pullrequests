662e9cfbf77af1e5cea6d77e2288597ba1e5fdd9
Peter Mellett <pjm@petermellett.co.uk>
2015-11-28 18:58:59 +0000

    missing migrations means bad schema.rb after merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122220446) do
+ActiveRecord::Schema.define(version: 20151127190033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,8 @@ ActiveRecord::Schema.define(version: 20151122220446) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.boolean  "inactive"
+    t.boolean  "featured",                  default: false
+    t.string   "avatar_url"
   end
 
   create_table "pull_request_archives", force: :cascade do |t|
