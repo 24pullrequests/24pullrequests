6a20ab6d4ec954c5c06f2cb2ab237d2172d937f0
Tim Perry <pimterry@gmail.com>
2015-12-21 16:51:19 +0100

    Refactor SQL to arel for aggregation filter

--- app/models/aggregation_filter.rb
+++ app/models/aggregation_filter.rb
@@ -1,9 +1,7 @@
 class AggregationFilter < ActiveRecord::Base
   belongs_to :user
 
-  PULL_REQUEST_SQL_FILTER = 'NOT EXISTS (
-    SELECT 1 FROM aggregation_filters filter
-    WHERE pull_requests.user_id = filter.user_id
-    AND pull_requests.repo_name LIKE filter.repo_pattern
-  )'
+  PULL_REQUEST_FILTER = AggregationFilter.where("pull_requests.user_id = aggregation_filters.user_id")
+                                         .where("pull_requests.repo_name LIKE aggregation_filters.repo_pattern")
+                                         .exists.not
 end
