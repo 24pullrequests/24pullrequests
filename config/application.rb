8755c3acc80f09e8b394bb80c57bc2ab0c68c155
Adam Yeats <ay@xadamy.xyz>
2016-11-26 00:21:44 +0000

    Update application.rb with new Rails5 require lines

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-# Pick the frameworks you want:
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'sprockets/railtie'
-require 'open-uri'
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
