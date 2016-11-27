8e5eea68eb6505705640e8fa7c71deb0b2442406
Andrew Nesbitt <andrewnez@gmail.com>
2016-11-27 22:27:23 +0000

    Renamed and merged some counter helpers

--- app/helpers/pull_request_helper.rb
+++
@@ -1,6 +0,0 @@
-module PullRequestHelper
-  def pull_request_count
-    return pull_request_count_for_language if @language
-    PullRequest.year(CURRENT_YEAR).count
-  end
-end
--- app/helpers/user_helper.rb
+++
@@ -1,6 +0,0 @@
-module UserHelper
-  def user_count
-    return user_count_for_language if @language
-    User.count
-  end
-end
