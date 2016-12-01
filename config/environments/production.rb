3eb99be26bdfab8bca6cb6f857a76c8008123e35
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-01 15:26:42 +0000

    Force ssl in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Tfpullrequests::Application.configure do
   config.action_dispatch.x_sendfile_header = nil
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   config.log_level = :debug
