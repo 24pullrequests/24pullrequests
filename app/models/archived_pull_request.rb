fddbce1189bc99830054fde4483a05119faf972c
Takashi Takebayashi <changesworlds@gmail.com>
2016-12-03 01:24:39 +0900

    Add 2015 to previous archived years

--- app/models/archived_pull_request.rb
+++ app/models/archived_pull_request.rb
@@ -1,5 +1,5 @@
 class ArchivedPullRequest < ApplicationRecord
-  PAST_YEARS = [2014, 2013, 2012]
+  PAST_YEARS = [2015, 2014, 2013, 2012]
 
   belongs_to :user
   scope :year, -> (year) { where('EXTRACT(year FROM "created_at") = ?', year) }
