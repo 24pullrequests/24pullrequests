37117705fadac4fd38d096658868bb9e009e8451
Hemant Gowda <hemant.gowda@cybrilla.com>
2016-12-15 10:59:02 +0530

    Rake task to move archived prs to prs

--- db/migrate/20161214123111_drop_archived_pull_requests.rb
+++
@@ -1,5 +0,0 @@
-class DropArchivedPullRequests < ActiveRecord::Migration[5.0]
-  def change
-  	drop_table :archived_pull_requests
-  end
-end
--- script/copy_archived_prs_to_prs.rb
+++
@@ -1,8 +0,0 @@
-# db/scripts/copy_archived_prs_to_prs.rb
-require 'rubygems'
-
-#to move all the archived_pull_requests back into the pull_requests table
-
-ArchivedPullRequest.all.find_each do |archived_pr|
-	PullRequest.create(archived_pr.attributes.except('id')).save!
-end
