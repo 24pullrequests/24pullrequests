fb8409ec35dcf6d285f556997ff43943a1ba4109
Koichi ITO <koic.ito@gmail.com>
2018-09-04 17:40:53 +0900

    Suppress "Delegating exists to arel is deprecated" warning
    This commit suppresses the following Active Record warnings.
    
    ```console
    DEPRECATION WARNING: Delegating exists to arel is deprecated and will be
    removed in Rails 6.0. (called from pull_request_filter at
    /Users/koic/src/github.com/24pullrequests/24pullrequests/app/models/aggregation_filter.rb:7)
    
    DEPRECATION WARNING: Delegating with to arel is deprecated and will be
    removed in Rails 6.0. (called from block (4 levels) in <top (required)>
    at /Users/koic/src/github.com/24pullrequests/24pullrequests/spec/controllers/users_controller_spec.rb:16)
    ```
    
    This patch is based on the following comment.
    https://github.com/rails/rails/pull/29619#issuecomment-392583498

--- app/models/aggregation_filter.rb
+++ app/models/aggregation_filter.rb
@@ -4,6 +4,6 @@ class AggregationFilter < ApplicationRecord
   def self.pull_request_filter
     where("pull_requests.user_id = aggregation_filters.user_id")
     .where("pull_requests.title ILIKE aggregation_filters.title_pattern")
-    .exists.not
+    .arel.exists.not
   end
 end
