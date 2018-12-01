0c2e7e52d39988a55222d24ef483810561ef586f
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-12-01 16:34:11 +0000

    Write spec for rake seeds task

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
 # Create some test data for development environments.
-if Rails.env.development?
+if Rails.env.development? || Rails.env.test?
 
   User.delete_all
   Contribution.delete_all
@@ -34,7 +34,7 @@ if Rails.env.development?
 
   Rails.logger.info 'Inserting some test data'
 
-  USERS.times do
+  USERS.times do |i|
     user = create :user, provider: 'developer2', gravatar_id: GRAVATARS.sample
     2.times { user.organisations << create(:organisation, avatar_url: "https://1.gravatar.com/avatar/#{ORGGRAVATARS.sample}") rescue nil }
 
@@ -50,7 +50,7 @@ if Rails.env.development?
   end
 
   PROJECTS.times do
-    project = create :project
+    project = create :project, submitted_by: User.take(1).first
     project.labels << labels.sample(2)
   end
 
