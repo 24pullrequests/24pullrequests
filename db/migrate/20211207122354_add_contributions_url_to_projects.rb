170e644aa35c1d6e79561f6e92a14bc8486a9389
Erin McLaughlin <emclaughlin@bostondigital.com>
2021-12-08 10:11:35 -0500

    Rolling back to one db migration.

--- db/migrate/20211207122354_add_contributions_url_to_projects.rb
+++ db/migrate/20211207122354_add_contributions_url_to_projects.rb
@@ -1,5 +1,5 @@
 class AddContributionsUrlToProjects < ActiveRecord::Migration[5.2]
   def change
-    add_column :projects, :contributions_url, :string
+    add_column :projects, :contributing_url, :string
   end
 end
--- db/migrate/20211207132134_rename_contributions_url.rb
+++
@@ -1,5 +0,0 @@
-class RenameContributionsUrl < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :projects, :contributions_url, :contributing_url
-  end
-end
