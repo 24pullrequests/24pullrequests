4a080b7223ebea1fccc48f477c90311a33d69107
Andrew Nesbitt <andrewnez@gmail.com>
2021-12-11 17:09:06 +0000

    Fix spec

--- spec/services/popularity_scorer_spec.rb
+++ spec/services/popularity_scorer_spec.rb
@@ -8,27 +8,27 @@ describe PopularityScorer do
 
   describe '#score' do
     it 'returns an integer score of project popularity' do
-      expect(project).to receive(:repo).and_return(double(updated_at: 6.months.ago))
+      expect(project).to receive(:repository).and_return(double(updated_at: 6.months.ago))
       expect(project).to receive(:issues).and_return([])
       expect(popularity_scorer.score).to be(0)
     end
 
     it 'returns a score of 5 if there are recent commits' do
-      expect(project).to receive(:repo).and_return(double(updated_at: Time.now))
+      expect(project).to receive(:repository).and_return(double(updated_at: Time.now))
       expect(project).to receive(:issues).and_return([])
       expect(popularity_scorer.score).to eq(5)
     end
 
     it 'returns a score of 3 if there are 5 recent issues' do
       issue = double(:issue)
-      expect(project).to receive(:repo).and_return(double(updated_at: 6.months.ago))
+      expect(project).to receive(:repository).and_return(double(updated_at: 6.months.ago))
       expect(project).to receive(:issues).and_return([issue] * 5)
       expect(popularity_scorer.score).to eq(3)
     end
 
     it 'does not give more than the maximum points' do
       issue = double(:issue)
-      expect(project).to receive(:repo).and_return(double(updated_at: 6.months.ago))
+      expect(project).to receive(:repository).and_return(double(updated_at: 6.months.ago))
       expect(project).to receive(:issues).and_return([issue] * 40)
       expect(popularity_scorer.score).to eq(10)
     end
