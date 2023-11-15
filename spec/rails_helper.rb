4917a091019acc769951f422e8fbfa6b0bbd2cbc
Andrew Nesbitt <andrewnez@gmail.com>
2023-11-15 15:31:53 +0000

    Fix tests in rails 7.1.2

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,9 +4,10 @@ SimpleCov.start 'rails'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 
-require 'rails/application'
+require 'rails/all'
 
 require File.expand_path('../../config/environment', __FILE__)
+
 require 'rspec/rails'
 require 'rspec/active_model/mocks'
 require 'rspec/its'
