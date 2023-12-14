817f0b2502c424afed1c6a6936871ccf9835d7d0
tbello <bello4aus@gmail.com>
2023-12-14 19:37:07 +0100

    fix(layout): change base url of projects label to github.com

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_12_12_143544) do
+ActiveRecord::Schema[7.1].define(version: 2021_12_12_143544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
