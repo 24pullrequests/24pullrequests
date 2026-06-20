f159773464c49bd43104936522b0458e34290e53
abhinavgautam01 <abgautam1017@gmail.com>
2026-06-20 21:11:19 +0530

    Keep event map helper scoped

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,143 +1,146 @@
-function Map(mapElement) {
-  this.mapElement = mapElement;
-  this.view = new ol.View({
-    center: ol.proj.transform([0, 18], "EPSG:4326", "EPSG:3857"),
-    zoom: 2
-  });
-
-  this.map = new ol.Map({
-    layers: [
-      new ol.layer.Tile({ source: new ol.source.OSM() })
-    ],
-    target: this.mapElement,
-    view: this.view
-  });
-
-  var latitude = $(this.mapElement).data("latitude");
-  var longitude = $(this.mapElement).data("longitude");
-
-  if (latitude != null && longitude != null) {
-    latitude = parseFloat(latitude, 10);
-    longitude = parseFloat(longitude, 10);
-    this.zoomTo(latitude, longitude, 14);
-    this.setupPicker(latitude, longitude);
-  } else {
-    this.setLocationToGeolocation();
-  }
-}
-
-Map.prototype.setupPicker = function(latitude, longitude) {
-  var self = this;
-
-  if ($(this.mapElement).data("map-picker")) {
-    this.pin = new ol.Overlay({
-      position: ol.proj.transform([longitude, latitude], "EPSG:4326", "EPSG:3857"),
-      element: $(".pin")[0]
+(function() {
+  function EventMap(mapElement) {
+    this.mapElement = mapElement;
+    this.view = new ol.View({
+      center: ol.proj.transform([0, 18], "EPSG:4326", "EPSG:3857"),
+      zoom: 2
     });
 
-    this.map.addOverlay(this.pin);
-
-    this.setForm(latitude, longitude);
-
-    this.map.on("click", function(evt) {
-      console.log(evt);
-      var coordinate = evt.coordinate;
-      self.pin.setPosition(evt.coordinate);
-      var latlng = ol.proj.transform(coordinate, "EPSG:3857", "EPSG:4326");
-      self.setForm(latlng[0], latlng[1]);
+    this.map = new ol.Map({
+      layers: [
+        new ol.layer.Tile({ source: new ol.source.OSM() })
+      ],
+      target: this.mapElement,
+      view: this.view
     });
-  }
-};
 
-Map.prototype.setForm = function(latitude, longitude) {
-  $("#event_longitude").val(latitude);
-  $("#event_latitude").val(longitude);
-};
+    var latitude = $(this.mapElement).data("latitude");
+    var longitude = $(this.mapElement).data("longitude");
 
-Map.prototype.zoomTo = function(latitude, longitude, zoom) {
-  if (zoom == null) {
-    zoom = 12;
-  }
-
-  this.view.setCenter(ol.proj.transform([longitude, latitude], "EPSG:4326", "EPSG:3857"));
-  this.view.setZoom(zoom);
-};
-
-Map.prototype.locationSuccess = function(position) {
-  this.zoomTo(position.coords.latitude, position.coords.longitude);
-  this.setupPicker(position.coords.latitude, position.coords.longitude);
-};
-
-Map.prototype.locationFail = function() {
-  this.setupPicker(0, 0);
-};
-
-Map.prototype.setLocationToGeolocation = function() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      this.locationSuccess.bind(this),
-      this.locationFail.bind(this)
-    );
-  } else {
-    this.locationFail();
-  }
-};
-
-$(function() {
-  var allowTimes = [];
-  var currentYear = (new Date()).getFullYear().toString();
-
-  for (var hour = 0; hour <= 23; hour++) {
-    for (var i = 0; i < ["00", "15", "30", "45"].length; i++) {
-      allowTimes.push(hour + ":" + ["00", "15", "30", "45"][i]);
+    if (latitude != null && longitude != null) {
+      latitude = parseFloat(latitude, 10);
+      longitude = parseFloat(longitude, 10);
+      this.zoomTo(latitude, longitude, 14);
+      this.setupPicker(latitude, longitude);
+    } else {
+      this.setLocationToGeolocation();
     }
   }
 
-  function configureDateTimePicker(selector, maxDate) {
-    $(selector).datetimepicker({
-      minDate: currentYear + "/12/01",
-      maxDate: maxDate,
-      startDate: currentYear + "/12/01",
-      allowTimes: allowTimes
-    });
-  }
+  EventMap.prototype.setupPicker = function(latitude, longitude) {
+    var self = this;
 
-  configureDateTimePicker(".js-datetimepicker", currentYear + "/12/25");
-  configureDateTimePicker(".js-contribution-datetimepicker", currentYear + "/12/24");
+    if ($(this.mapElement).data("map-picker")) {
+      this.pin = new ol.Overlay({
+        position: ol.proj.transform([longitude, latitude], "EPSG:4326", "EPSG:3857"),
+        element: $(".pin")[0]
+      });
 
-  $(".map").each(function() {
-    var map = new Map(this);
+      this.map.addOverlay(this.pin);
 
-    $(".js-pins .pin").each(function() {
-      var latitude = $(this).data("latitude");
-      var longitude = $(this).data("longitude");
+      this.setForm(latitude, longitude);
 
-      if (latitude != null && longitude != null) {
-        latitude = parseFloat(latitude, 10);
-        longitude = parseFloat(longitude, 10);
+      this.map.on("click", function(evt) {
+        console.log(evt);
+        var coordinate = evt.coordinate;
+        self.pin.setPosition(evt.coordinate);
+        var latlng = ol.proj.transform(coordinate, "EPSG:3857", "EPSG:4326");
+        self.setForm(latlng[0], latlng[1]);
+      });
+    }
+  };
 
-        var overlay = new ol.Overlay({
-          position: ol.proj.transform([longitude, latitude], "EPSG:4326", "EPSG:3857"),
-          element: this
-        });
+  EventMap.prototype.setForm = function(latitude, longitude) {
+    $("#event_longitude").val(latitude);
+    $("#event_latitude").val(longitude);
+  };
 
-        map.map.addOverlay(overlay);
+  EventMap.prototype.zoomTo = function(latitude, longitude, zoom) {
+    if (zoom == null) {
+      zoom = 12;
+    }
+
+    this.view.setCenter(ol.proj.transform([longitude, latitude], "EPSG:4326", "EPSG:3857"));
+    this.view.setZoom(zoom);
+  };
+
+  EventMap.prototype.locationSuccess = function(position) {
+    this.zoomTo(position.coords.latitude, position.coords.longitude);
+    this.setupPicker(position.coords.latitude, position.coords.longitude);
+  };
+
+  EventMap.prototype.locationFail = function() {
+    this.setupPicker(0, 0);
+  };
+
+  EventMap.prototype.setLocationToGeolocation = function() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this.locationSuccess.bind(this),
+        this.locationFail.bind(this)
+      );
+    } else {
+      this.locationFail();
+    }
+  };
+
+  $(function() {
+    var allowTimes = [];
+    var currentYear = (new Date()).getFullYear().toString();
+    var quarters = ["00", "15", "30", "45"];
+
+    for (var hour = 0; hour <= 23; hour++) {
+      for (var i = 0; i < quarters.length; i++) {
+        allowTimes.push(hour + ":" + quarters[i]);
       }
+    }
+
+    function configureDateTimePicker(selector, maxDate) {
+      $(selector).datetimepicker({
+        minDate: currentYear + "/12/01",
+        maxDate: maxDate,
+        startDate: currentYear + "/12/01",
+        allowTimes: allowTimes
+      });
+    }
+
+    configureDateTimePicker(".js-datetimepicker", currentYear + "/12/25");
+    configureDateTimePicker(".js-contribution-datetimepicker", currentYear + "/12/24");
+
+    $(".map").each(function() {
+      var map = new EventMap(this);
+
+      $(".js-pins .pin").each(function() {
+        var latitude = $(this).data("latitude");
+        var longitude = $(this).data("longitude");
+
+        if (latitude != null && longitude != null) {
+          latitude = parseFloat(latitude, 10);
+          longitude = parseFloat(longitude, 10);
+
+          var overlay = new ol.Overlay({
+            position: ol.proj.transform([longitude, latitude], "EPSG:4326", "EPSG:3857"),
+            element: this
+          });
+
+          map.map.addOverlay(overlay);
+        }
+      });
     });
+
+    if ($(".toggle-past-events").length) {
+      $(".toggle-past-events").click(function() {
+        var pastEvents = $(".past-events");
+        var spanTxt = $(".toggle-past-events").children("span");
+
+        if (pastEvents.hasClass("hidden")) {
+          pastEvents.removeClass("hidden");
+          spanTxt.text("Hide");
+        } else {
+          pastEvents.addClass("hidden");
+          spanTxt.text("Show");
+        }
+      });
+    }
   });
-
-  if ($(".toggle-past-events").length) {
-    $(".toggle-past-events").click(function() {
-      var pastEvents = $(".past-events");
-      var spanTxt = $(".toggle-past-events").children("span");
-
-      if (pastEvents.hasClass("hidden")) {
-        pastEvents.removeClass("hidden");
-        spanTxt.text("Hide");
-      } else {
-        pastEvents.addClass("hidden");
-        spanTxt.text("Show");
-      }
-    });
-  }
-});
+}());
